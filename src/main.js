cf904b8351a036e38a669c4901fcdc22bce7ea5f
Yury Chebunin <yachebunin@gmail.com>
2021-11-11 20:31:43 +0300

    added vue apollo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import { createProvider } from './vue-apollo'
+import 'core-js/stable'; // only stable feature also is possible with only `core-js`
+import 'regenerator-runtime/runtime'; // To ensure that regeneratorRuntime is defined globally
 
 Vue.config.productionTip = false
 
@@ -10,5 +13,6 @@ new Vue({
   router,
   store,
   vuetify,
+  apolloProvider: createProvider(),
   render: function (h) { return h(App) }
 }).$mount('#app')
